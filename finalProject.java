60ccc4a7da4fa59ab8278484897fb2b3ad7a2ac7
DorisChang <doris900318@gmail.com>
2018-06-04 13:13:42 -0400

    With 2D array

--- finalProject.java
+++ finalProject.java
@@ -8,6 +8,7 @@ import javax.swing.Timer;
 import java.util.Hashtable;
 import java.io.*;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class finalProject extends JFrame implements ActionListener{
 	static final Color SOFT_BLOCK_COLOUR = new Color(120,190,120);
@@ -20,8 +21,6 @@ public class finalProject extends JFrame implements ActionListener{
 	
 	public finalProject(){
 		super("Bomberman");
-		//setSize(480,460); //440 y screen size
-		//setSize(960,920);
 		
 		setSize(720,690);
 		myTimer = new Timer(60,this);
@@ -56,7 +55,8 @@ class GamePanel extends JPanel implements KeyListener{
 	private int sx; //screen movement
 	
 	public static final int RIGHT = 1, LEFT = 2, UP = 3, DOWN = 4;
-
+	
+	private int grid[][] = new int[13][27];
 	private HashTable<Block> hardBlocks = new HashTable<Block>();
 	private HashTable<Block> softBlocks = new HashTable<Block>();
 	// private HashTable<Integer, Block> hardBlocks = new HashTable<Integer, Block>();
@@ -70,8 +70,9 @@ class GamePanel extends JPanel implements KeyListener{
 		setFocusable(true);
 		addKeyListener(this);
 		requestFocus();
-		addSoftBlocks(softBlocks);
-		
+		newGrid(grid);
+		addSoftBlocks(50);
+		//System.out.println(Arrays.deepToString(grid));
 		
 		mx = 0;
 		my = 0;
@@ -96,7 +97,7 @@ class GamePanel extends JPanel implements KeyListener{
 		
 		//System.out.println(allMonsters.get(0));
 		//for(int i=0;i<5; i++){
-			addMonster(0);
+			//addMonster(0);
 		//	}
 		
 			
@@ -117,7 +118,8 @@ class GamePanel extends JPanel implements KeyListener{
 
 		//BORDERS
 		//Horizontal Borders
-		for(int i = 0; i < 1275; i +=45){
+		//int n =0;
+		/*for(int i = 0; i < 1275; i +=45){
 			g.setColor(finalProject.HARD_BLOCK_COLOUR);
 			g.fillRect(i+mx,65,45,45);
 			g.fillRect(i+mx,605,45,45);
@@ -125,7 +127,9 @@ class GamePanel extends JPanel implements KeyListener{
 			g.setColor(new Color(255,0,0));
 			g.drawRect(i+mx,65,45,45);
 			g.drawRect(i+mx,605,45,45);
+			//n++;
 		}
+		//System.out.printf("number: %d\n",n);
 		
 		//Vertical Borders
 		for(int i = 110; i < 745; i +=45){
@@ -150,7 +154,26 @@ class GamePanel extends JPanel implements KeyListener{
 				g.setColor(new Color(255,0,0));
 				g.drawRect(i+mx,j,45,45);
 			}
-		}
+		}*/
+		
+		for(int r=0; r<13; r++){ //row
+			for(int c=0; c<27; c++){ //column
+				if(grid[r][c]==1){
+					g.setColor(finalProject.HARD_BLOCK_COLOUR);
+					g.fillRect(c*45+mx,r*45+65,45,45);
+
+					g.setColor(new Color(255,0,0));
+					g.drawRect(c*45+mx,r*45+65,45,45);
+					}
+				if(grid[r][c]==2){
+					g.setColor(finalProject.SOFT_BLOCK_COLOUR);
+					g.fillRect(c*45+mx,r*45+65,45,45);
+
+					g.setColor(new Color(255,0,0));
+					g.drawRect(c*45+mx,r*45+65,45,45);
+					}
+				}
+			}
 		
 		//SOFTBLOCKS
 		ArrayList<Block> sBlocks = softBlocks.toArray();
@@ -175,9 +198,9 @@ class GamePanel extends JPanel implements KeyListener{
 
 	public void start(){
 		moveMan();
-		for(Monster m:monsters){
-			moveMonster(m);
-			}
+		//for(Monster m:monsters){
+		//	moveMonster(m);
+		//	}
 	}
 
 	//ALL THREE MUST BE PRESENT
@@ -191,12 +214,22 @@ class GamePanel extends JPanel implements KeyListener{
 
 	public void keyTyped(KeyEvent e){}
 
-	public boolean hitBlock(int direction){
+	/*public boolean hitBlock(int direction){
 		if(hardBlocks.collidesBlock((p.getX()+sx)*1000+p.getY(),direction)!= true && softBlocks.collidesBlock((p.getX()+sx)*1000+p.getY(),direction)!= true){
 			return false;
 			}
 		return true;
-	}
+	}*/
+	
+	public Boolean hitBlock(int direction){
+		
+		double c = (p.getX()+15.5)/45; //closest column
+		double r = (p.getY()+15.5-65)/45; //closest row
+		System.out.printf("column: %f, row: %f\n", c,r);
+		//if(direction == RIGHT){
+		//	 }
+		return false;
+		}
 
 	public void moveMan(){
 		if(keys[KeyEvent.VK_RIGHT]){
@@ -239,27 +272,50 @@ class GamePanel extends JPanel implements KeyListener{
 		}
 	}
 	
-	public void addSoftBlocks(HashTable <Block> blocks){
+	public void newGrid(int grid[][]){ //resets the 2D grid of the map with the borders and hardblocks
+		for(int row=0; row<13; row++){ //clear the grid to be just zeros
+			Arrays.fill(grid[row],0);
+			}
+		for(int x = 0; x<=26; x++){ //add the top and bottom borders
+			for(int y = 0; y<13; y+=12){
+				grid[y][x]=1;
+				}
+			}
+		for(int x = 0; x<27; x+=26){ //add the left and right borders
+			for(int y = 0; y<13; y+=1){
+				grid[y][x]=1;
+				}
+			} 
+		for(int x = 2; x<25; x+=2){ //add the hardblocks in the middle
+			for(int y = 2; y<11; y+=2){
+				grid[y][x]=1;
+				}
+		}
+	}
+	
+	public void addSoftBlocks(int num){
 		Random rand = new Random();
-		for(int n = 0; n<40; n++){ 
+		for(int n = 0; n<num; n++){ 
 			int randX = rand.nextInt(24)+1; //randomly generated X 
 			int randY = rand.nextInt(10)+1; //randomly generated Y
 			if (randX%2 == 0){ //makes sure that the randomly generated spot is not occupied by a hardblock
-				randY+=(randY%2==1?1:0);
+				randY+=(randY%2==0?1:0);
 				}
 			if(randX == 2 && randY == 0 ||  randX == 1 && randY == 1 || randX==1 && randY ==0){
-				addSoftBlocks(blocks);
+				addSoftBlocks(1);
 				}
 			else{
-				blocks.add(new Block(""+randX*45+","+(randY*45+110)+",45,45"));	
+				//blocks.add(new Block(""+randX*45+","+(randY*45+110)+",45,45"));
+				grid[randY][randX]=2;	
 				}
 			//System.out.printf("randX: %d, randY: %d\n",randX,randY);
 			}
-			
 		//System.out.println(blocks.toArray().size());
 		}
+		
+		
 	
-	public void addMonster(int type){
+	/*public void addMonster(int type){
 		Random rand = new Random();
 		int randX = rand.nextInt(24)+1; //randomly generated X 
 		int randY = rand.nextInt(10)+1; //randomly generated Y
@@ -285,5 +341,8 @@ class GamePanel extends JPanel implements KeyListener{
 		
 	public void moveMonster(Monster m){
 		softBlocks.validDirections(m);
-		}
+		//rx = (m.getX()-7)/45; //row #
+		//ry = (m.getY()-7)/45; //row #
+		//if()
+		}*/
 }
--- testing.java
+++ testing.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.image.*;
 import javax.swing.Timer;
+import java.util.Arrays;
 
 import java.io.*;
 
@@ -15,16 +16,42 @@ public class testing {
 		softBlocks.add(new Block("330,70,60,60"));
 		softBlocks.add(new Block("350,70,60,60"));
 		softBlocks.add(new Block("330,70,60,60"));
-		System.out.println(softBlocks.toArray());
+		//System.out.println(softBlocks.toArray());
 		softBlocks.clear();
-		System.out.println(softBlocks.toArray());
+		//System.out.println(softBlocks.toArray());
 		HashTable<String> words = new HashTable<String>();
 		words.add("hello");
 		words.add("who?");
 		words.add("what?");
-		System.out.println(words.toArray());
+		//System.out.println(words.toArray());
 		words.clear();
-		System.out.println(words.toArray());
+		//System.out.println(words.toArray());
+		
+		
+		int grid[][] = new int[13][27];
+		grid[1][4]=1;
+		System.out.println(Arrays.deepToString(grid));
+		for(int row=0; row<13; row++){
+			Arrays.fill(grid[row],0);
+			}
+		for(int x = 0; x<=26; x++){
+			for(int y = 0; y<13; y+=12){
+				grid[y][x]=1;
+				}
+			}
+		for(int x = 0; x<27; x+=26){
+			for(int y = 0; y<13; y+=1){
+				grid[y][x]=1;
+				}
+			} 
+		for(int x = 2; x<25; x+=2){
+			for(int y = 2; y<11; y+=2){
+				grid[y][x]=1;
+				}
+			}
+		//for(int x = )
+		//System.out.println(Arrays.deepToString(grid));
+		//System.out.println(grid[2][4]);
 	}
     
     
